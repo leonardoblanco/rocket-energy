5b46c27b653242f3be0fbe99c2dc32bebc2e246d
loliveira <loliveira@192.168.0.107>
2019-06-02 18:20:12 -0300

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![hello])
+        .launch();
+}
